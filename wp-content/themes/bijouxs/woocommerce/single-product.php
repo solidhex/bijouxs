03a7545bce403e357714bf45bbc771fb1474606b
solidhex <patrick.jackson@ff0000.com>
2013-12-06 16:43:55 -0800

    single product view

--- wp-content/themes/bijouxs/woocommerce/single-product.php
+++ wp-content/themes/bijouxs/woocommerce/single-product.php
@@ -13,15 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 get_header('shop'); ?>
 
-	<?php
-		/**
-		 * woocommerce_before_main_content hook
-		 *
-		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-		 * @hooked woocommerce_breadcrumb - 20
-		 */
-		do_action('woocommerce_before_main_content');
-	?>
+
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
